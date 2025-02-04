9f22dc9701efc5eeef54328deb4d898ce8920ab6
ivatree <ivan.9-0.z@mail.ru>
2025-02-04 17:46:45 +0300

    sorting by price and name, added new pizza-card, a little bug fix

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,19 +1,32 @@
 import React, { useState } from 'react';
 import './styles.scss';
 import Button from '../Button';
-import { Menu } from '../SortList';
+import { Menu } from './SortList';
+
 import { AiOutlineCaretDown, AiOutlineCaretUp } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
+import { Choise } from './ChoiseList';
 
 export default function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
   const [selectedOption, setSelectedOption] = useState('popular');
-
+  const [selectedChoiseOption, setSelectedChoiseOption] = useState('all');
+  const navigate = useNavigate();
   const arrow = openMenu ? <AiOutlineCaretUp /> : <AiOutlineCaretDown />;
 
-  const options: { [key: string]: string } = {
-    popular: 'популярности',
-    cost: 'цене',
-    alphabet: 'алфавиту',
+  const options = {
+    popular: {
+      title: 'популярности',
+      url: () => navigate('/'),
+    },
+    cost: {
+      title: 'цене',
+      url: () => navigate('/Price'),
+    },
+    alphabet: {
+      title: 'алфавиту',
+      url: () => navigate('/Name'),
+    },
   };
 
   const handleOptionChange = (option: string) => {
@@ -21,34 +34,26 @@ export default function Navbar() {
     setOpenMenu(false);
   };
 
+  const choiceOptions = {
+    all: { title: 'Все' },
+    meat: { title: 'Мясные' },
+    vegetarian: { title: 'Вегетарианские' },
+    grill: { title: 'Гриль' },
+    spisy: { title: 'Острые' },
+  };
+
+  const handleChoiseChange = (choiseOption: string) => {
+    setSelectedChoiseOption(choiseOption);
+  };
+
   return (
     <>
       <nav className="settings-container">
         <div className="choise-container">
-          <Button
-            className="choise"
-            onClick={() => console.log()}
-            text={'Все'}
-          />
-          <Button
-            className="choise"
-            onClick={() => console.log()}
-            text={'Мясные'}
-          />
-          <Button
-            className="choise"
-            onClick={() => console.log()}
-            text={'Вегетарианские'}
-          />
-          <Button
-            className="choise"
-            onClick={() => console.log()}
-            text={'Гриль'}
-          />
-          <Button
-            className="choise"
-            onClick={() => console.log()}
-            text={'Острые'}
+          <Choise
+            selectedChoiseOption={selectedChoiseOption}
+            choiseOptions={choiceOptions}
+            onChoiseChange={handleChoiseChange}
           />
         </div>
         <div className="sort-settings">
@@ -59,7 +64,7 @@ export default function Navbar() {
               <>
                 {arrow} Сортировка по:{' '}
                 <span className="selected-option">
-                  {options[selectedOption]}
+                  {options[selectedOption].title}
                 </span>
               </>
             }
